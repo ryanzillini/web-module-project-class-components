1c34e3b92a440dd294af7aff02bb4e6aa95a9444
Ryan Zillini <nyrtrading134@gmail.com>
2023-05-30 12:02:46 -0400

    built state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: "Organize Garage",
+          id: 10, // could look different, you could use a timestamp to generate it
+          completed: false,
+        },
+        {
+          task: "Bake Cookies",
+          id: 20,
+          completed: false,
+        },
+      ],
+    };
+  }
+
   render() {
+    const { todos } = this.state;
     return (
       <div>
-        Todo App
+        <h1>ToDos</h1>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                {todo.task}
+                {todo.completed ? <span>- completed</span> : <span></span>}
+              </li>
+            );
+          })}
+        </ul>
+
+        <form>
+          <input />
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
-    )
+    );
   }
 }
